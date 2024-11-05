8d9b88815e5105a788e08ca51d331b36e051d4ad
VankotU <131190241+VankotU@users.noreply.github.com>
2024-11-05 12:12:36 +0200

    Add files via upload

---
+++ TaskImage/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try 
+                {
+                    this.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+                    this.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+                catch 
+                {
+                    MessageBox.Show("Problem with file");
+                }
+            }
+        }
+    }
+}
