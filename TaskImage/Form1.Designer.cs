4e7f88d2e76459a3168cbd8ed519736b591e0f18
VankotU <131190241+VankotU@users.noreply.github.com>
2024-11-06 10:30:44 +0200

    Add files via upload
    Podobrena versiq:)

--- TaskImage/Form1.Designer.cs
+++ TaskImage/Form1.Designer.cs
@@ -30,15 +30,20 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSaving = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(189, 117);
+            this.button1.Location = new System.Drawing.Point(12, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Смяна фон";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -46,15 +51,51 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(563, 316);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 148);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(124, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Търсене на док";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSaving
+            // 
+            this.btnSaving.Location = new System.Drawing.Point(12, 177);
+            this.btnSaving.Name = "btnSaving";
+            this.btnSaving.Size = new System.Drawing.Size(124, 23);
+            this.btnSaving.TabIndex = 3;
+            this.btnSaving.Text = "Съхраняване";
+            this.btnSaving.UseVisualStyleBackColor = true;
+            this.btnSaving.Click += new System.EventHandler(this.btnSaving_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.ClientSize = new System.Drawing.Size(786, 340);
+            this.Controls.Add(this.btnSaving);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,6 +103,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSaving;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- TaskImage/Form1.cs
+++ TaskImage/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TaskImage
 {
     public partial class Form1 : Form
@@ -15,6 +16,10 @@ namespace TaskImage
         public Form1()
         {
             InitializeComponent();
+            btnSearch.Click += btnSearch_Click;
+            btnSaving.Click += btnSaving_Click;
+            openFileDialog2.Filter = "Text files(*.txt)|*.txt|All files(*,*)|*,*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*,*)|*,*";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,6 +36,28 @@ namespace TaskImage
                     MessageBox.Show("Problem with file");
                 }
             }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+            
+                return;
+                string fileName = openFileDialog2.FileName;
+                string fileText = System.IO.File.ReadAllText(fileName);
+                textBox1.Text = fileText;
+                MessageBox.Show("File was found");
+            
+        }
+
+        private void btnSaving_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+
+            return;
+            string fileName = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(fileName,textBox1.Text);
+            MessageBox.Show("File was saved");
         }
     }
 }
